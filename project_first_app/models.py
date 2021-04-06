06e11b8b073add7d84e2928338c4d124c5f3d722
Your Name <you@example.com>
2021-04-06 16:42:09 +0300

    models.py reworked

--- project_first_app/models.py
+++ project_first_app/models.py
@@ -12,6 +12,7 @@ class Owner(models.Model):
     first_name = models.CharField(max_length=30)
     second_name = models.CharField(max_length=30)
     birth_date = models.DateTimeField()
+    ownerships = models.ManyToManyField(Car, through='Ownership')
 
 
 class License(models.Model):
@@ -22,7 +23,7 @@ class License(models.Model):
 
 
 class Ownership(models.Model):
-    owner_ownership = models.ForeignKey(Owner, on_delete=models.CASCADE)
-    car_ownership = models.ForeignKey(Car, on_delete=models.CASCADE)
+    ownerships = models.ForeignKey(Owner, on_delete=models.CASCADE)
+    carownership = models.ForeignKey(Car, on_delete=models.CASCADE)
     date_start = models.DateTimeField()
     date_ending = models.DateTimeField()
